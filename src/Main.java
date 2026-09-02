d88ee3ee4ee278cd235ba560f56baa8800a44b04
juazocar <juazocar@profesor.duoc.cl>
2026-09-02 01:19:19 -0400

    Se agrega constructor

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,27 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        for (int i = 1; i <= 5; i++) {
-            System.out.println("Procesando estudiante " + i);
+        Scanner scanner = new Scanner(System.in);
+
+        int opcion = -1;
+
+        while (opcion != 0) {
+
+            System.out.println("=== SISTEMA DUOC ===");
+            System.out.println("1. Mostrar estado");
+            System.out.println("2. Procesar operación");
+            System.out.println("0. Salir");
+
+            System.out.print("Seleccione una opción: ");
+            opcion = scanner.nextInt();
+
+            if (opcion == 1) {
+                System.out.println("Sistema operativo.");
+            } else if (opcion == 2) {
+                System.out.println("Procesando operación...");
+            } else if (opcion == 0) {
+                System.out.println("Cerrando sistema...");
+            }
         }
     }
 }
